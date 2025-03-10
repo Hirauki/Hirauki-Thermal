5028e783a126c3ead79f5273b32346e3e9c6b6e7
Hirauki <120457320+Hirauki@users.noreply.github.com>
2025-03-11 01:20:42 +0700

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -97,10 +97,13 @@ ext 5500000 /sys/class/qcom-battery/restricted_current
 ext 5000000 /sys/class/power_supply/pc_port/current_max
 ext 5500000 /sys/class/power_supply/battery/constant_charge_current_max
 
-# Handle case when 'default_gov' is performance
-if echo $default_gov | grep -q performance; then
-	default_gov="schedutil"
+if [ -e /sys/class/kgsl/kgsl-3d0/devfreq/governor ]; then
+  echo "msm-adreno-tz" > /sys/class/kgsl/kgsl-3d0/devfreq/governor
 fi
+
+find /sys/devices/system/cpu -maxdepth 1 -name 'cpu?' | while IFS= read -r cpu; do
+  echo performance > "$cpu/cpufreq/scaling_governor"
+done
 sleep 10
 echo 0 > /sys/class/kgsl/kgsl-3d0/throttling
 echo 0 > /sys/class/kgsl/kgsl-3d0/bus_split
@@ -112,15 +115,6 @@ echo 1 > /sys/class/kgsl/kgsl-3d0/force_clk_on
 echo "com.miHoYo., com.activision., UnityMain, libunity.so, libil2cpp.so, libfb.so" > /proc/sys/kernel/sched_lib_name
 #write /proc/sys/kernel/sched_lib_mask_force 255
 echo "240" > /proc/sys/kernel/sched_lib_mask_force
-
-for path in /proc/sys/kernel/sched_lib_name /proc/sys/kernel/sched_lib_mask_force /proc/sys/walt/sched_lib_name /proc/sys/walt/sched_lib_mask_force; do
-    if [ -w "$path" ]; then
-        case "$path" in
-            */sched_lib_name) echo "$lib_names" > "$path" ;;
-            */sched_lib_mask_force) echo "255" > "$path" ;;
-        esac
-    fi
-done
 
 rm -f /storage/emulated/0/*.log;
 settings delete global device_idle_constants
@@ -162,20 +156,6 @@ echo "0" > "/proc/sys/kernel/panic_on_oops"
 echo "0" > "/proc/sys/kernel/panic"
 echo "0" > "/proc/sys/kernel/softlockup_panic"
 
-echo "0" > /sys/kernel/rcu_expedited
-echo "0" > /sys/kernel/rcu_normal
-echo "0" > /sys/devices/system/cpu/isolated
-echo "0" > /proc/sys/kernel/sched_tunable_scaling
-echo "1" > /proc/sys/kernel/timer_migration
-echo "55" > /proc/sys/kernel/perf_cpu_time_max_percent
-echo "1" > /proc/sys/kernel/sched_autogroup_enabled
-echo "0" > /proc/sys/kernel/sched_child_runs_first
-echo "10000000" > /proc/sys/kernel/sched_latency_ns 
-echo "2000000" > /proc/sys/kernel/sched_wakeup_granularity_ns 
-echo "3200000" > /proc/sys/kernel/sched_min_granularity_ns 
-echo "2000000" > /proc/sys/kernel/sched_migration_cost_ns 
-echo "32" > /proc/sys/kernel/sched_nr_migrate
-
 	for dir in /sys/block/mmcblk0 /sys/block/mmcblk1 /sys/block/sd*; do
 		# Reduce heuristic read-ahead in exchange for I/O latency
 		apply 32 "$dir/queue/read_ahead_kb"
@@ -190,9 +170,15 @@ for dir in /sys/block/mmcblk0 /sys/block/mmcblk1 /sys/block/sd*; do
     fi
 done
 
+####################################
+# Surfaceflinger
+####################################
 setprop debug.sf.hw 1
 setprop debug.sf.latch_unsignaled 1
 
+####################################
+#Decrease Render treat speed
+####################################
 change_task_cgroup "surfaceflinger" "top-app" "cpuset"
 change_task_cgroup "surfaceflinger" "foreground" "stune"
 change_task_cgroup "android.hardware.graphics.composer" "top-app" "cpuset"
@@ -204,10 +190,6 @@ change_task_affinity ".hardware.biometrics.fingerprint" "ff"
 change_task_affinity ".hardware.camera.provider" "ff"
 change_task_nice "system_server" "-18"
 change_task_nice "com.android.systemui" "-18"
-
-chmod 755 /sys/module/qti_haptics/parameters/vmax_mv_override
-echo 500 > /sys/module/qti_haptics/parameters/vmax_mv_override
-echo 0 > /sys/module/rmnet_data/parameters/rmnet_data_log_level
 
 fstrim -v /cache
 fstrim -v /system
